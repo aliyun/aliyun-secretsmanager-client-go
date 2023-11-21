a46d68130ca46682d2eb17def219f95dec45aca5
wanglixiang01 <105350613+wanglixiang01@users.noreply.github.com>
2023-11-21 15:10:51 +0800

    fix passord bug need strip newlines (#16)

--- sdk/utils/config_utils.go
+++ sdk/utils/config_utils.go
@@ -2,47 +2,45 @@ package utils
 
 import (
 	"bufio"
-	"io"
 	"os"
 	"strings"
 )
 
 func LoadProperties(fileName string) (map[string]string, error) {
 	properties := make(map[string]string)
-	if !FileExist(fileName) {
-		return nil, nil
-	}
-	srcFile, err := os.OpenFile(fileName, os.O_RDONLY, 0666)
-	defer srcFile.Close()
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0666)
 	if err != nil {
 		return nil, err
 	}
-	srcReader := bufio.NewReader(srcFile)
-	for {
-		str, err := srcReader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF && (0 == len(str) || str == "\n" || strings.HasPrefix(strings.TrimSpace(str), "#")) {
-				break
-			}
-		}
-		if 0 == len(str) || str == "\n" || strings.HasPrefix(strings.TrimSpace(str), "#") {
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		str := scanner.Text()
+		if strings.HasPrefix(str, "#") {
 			continue
 		}
-		if strings.HasSuffix(str, "\r\n") {
-			str = strings.Replace(str, "\r\n", "", -1)
-		} else if strings.HasSuffix(str, "\n") {
-			str = strings.Replace(str, "\n", "", -1)
+		strings.TrimSpace(str)
+		if str != "" {
+			property := strings.Split(str, "=")
+			if len(property) == 2 {
+				properties[strings.TrimSpace(property[0])] = strings.TrimSpace(property[1])
+			}
 		}
-		property := strings.Split(str, "=")
-		properties[strings.TrimSpace(property[0])] = strings.TrimSpace(property[1])
+	}
+	if err = scanner.Err(); err != nil {
+		return nil, err
 	}
 	return properties, nil
 }
 
-func FileExist(_path string) bool {
+func FileExist(_path string) (bool, error) {
 	_, err := os.Stat(_path)
-	if err != nil && os.IsNotExist(err) {
-		return false
+	if err == nil {
+		return true, nil
 	}
-	return true
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
